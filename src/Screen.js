dddeadb055979169890b6df313a5d3fc14403a89
Aaron G <aaronhgross@gmail.com>
2018-05-01 09:45:27 -0400

    starting form submission

--- src/Screen.js
+++ src/Screen.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
+import LoginPage from './LoginPage'
 
+let DumbHomepage = () =>
+<div>Dumb Homepage</div>
 
 let ScreenDumb = () => {   
     return(
         <div>
-            Working
+            <Router>
+                <Switch>
+                    <Route path="/" exact component={DumbHomepage} />
+                    <Route path="/login" exact component={LoginPage} />
+                </Switch>
+            </Router>
         </div>
     )
  }

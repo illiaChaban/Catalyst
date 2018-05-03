702c18e262b4ee8bd3de2ea90dbad1fb555e1942
Aaron G <aaronhgross@gmail.com>
2018-05-03 18:26:07 -0400

    added a hover effect for nav

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -17,10 +17,10 @@ let MainScreen = () => {
             <div className="nav-placeholder"></div>
             <nav className="navbar">
                 <ul>
-                    <li><NavLink to="/main/profile-page"><i className="fas fa-user fa-2x"></i></NavLink></li>
-                    <li><NavLink to="/main/feed"><i className="fas fa-list-ul fa-2x"></i></NavLink></li>
-                    <li><NavLink to="/main/create-goal"><i className="fas fa-clipboard-check fa-2x"></i></NavLink></li>
-                    <li><NavLink to="/main/friends"><i className="fas fa-users fa-2x"></i></NavLink></li>
+                    <li><NavLink to="/main/profile-page"><i className="fas fa-user fa-2x hvr-grow"></i></NavLink></li>
+                    <li><NavLink to="/main/feed"><i className="fas fa-list-ul fa-2x hvr-grow"></i></NavLink></li>
+                    <li><NavLink to="/main/create-goal"><i className="fas fa-clipboard-check fa-2x hvr-grow"></i></NavLink></li>
+                    <li><NavLink to="/main/friends"><i className="fas fa-users fa-2x hvr-grow"></i></NavLink></li>
                 </ul>
             </nav>
         </div>

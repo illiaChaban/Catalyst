c0c95a3dc43b6a3dd7da2d4cdb8e1838b6599efb
Illia Chaban <illia.chaban.8@gmail.com>
2018-05-03 23:54:28 -0400

    working profile page, no buttons add goal/checkin

--- src/ProfileCheckins.js
+++ src/ProfileCheckins.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import ProfileCheckin from './ProfileCheckin';
 
 class ProfileCheckins extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            checkins: []
+        }
+    }
+
+    componentDidUpdate(prevProps){
+        if( prevProps !== this.props) {
+            fetch('http://localhost:5000/getMyCheckins', {
+                method: 'POST',
+                body: JSON.stringify(this.props.user)
+            })
+            .then(res => res.json())
+            // .then(console.log)
+            .then( checkins => {
+                this.setState({checkins})
+            })
+        }
+    }
 
     render() {
-
+        let {checkins} = this.state;
         return(
-            <div>Profile Checkins</div>
+            <div className='profile-goals-part'>
+                <div className='profile-title'> Recent Checkins: </div>
+                {checkins.length && checkins.map( (chk, i) => {
+                    return <ProfileCheckin key={i} checkin={chk}/>
+                })}
+            </div>
         )
     }
 }

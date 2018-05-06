33c6e8b0f6b02a9d0bb65752f0d8c01ff255fc15
Illia Chaban <illia.chaban.8@gmail.com>
2018-05-05 21:52:27 -0400

    people can see punishments on the profile

--- src/ProfileCheckin.js
+++ src/ProfileCheckin.js
@@ -7,7 +7,7 @@ let ProfileCheckin = ({checkin}) => {
     if (created) time = created.slice(0,10) + ' ' + created.slice(11, 16);
     
     return (
-        <div>
+        <div className='profile-goal'>
             {image && <img className='inline' src={image} alt='checkin'/>}
             <div>{description}</div>
             <div className='checkin-created'>created: {time}</div>

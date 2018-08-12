caeb4ebaf7182de1fca83927e20e4ff03cd175ef
Illia Chaban <illia.chaban.8@gmail.com>
2018-08-12 01:50:00 -0400

    updated punishments

--- src/lib/punishments.js
+++ src/lib/punishments.js
@@ -1,8 +1,8 @@
 const punishments = [
     'run around the block in underwear',
-    "buy sushi for all of your Catalyst's friends",
+    "buy sushi for all of your Catalysts friends",
     'go to starbucks and ask to work for food',
-    'go to a gas station an wash people\'s cars, no tips',
+    'go to a gas station an wash peoples cars, no tips',
     'go outside and force people to take your autographs',
     'draw a sun on your forehead and go like that to work',
     'sell a t-shirt you wearing to random people on the street'
